9f73e5bafe1c6221996c24c6bbc9f64d0e008509
puspitarahmanpuspo <92968791+puspitarahmanpuspo@users.noreply.github.com>
2023-10-12 18:53:42 +0600

    Add files via upload

---
+++ Puspita/Day3.js
@@ -0,0 +1,91 @@
+window.alert("There Is No Result Of Js, Only showing html work because, Js File's Contents are in Comment.");
+alert("please remove comments from js file and checkout my practice");
+                    
+
+                            //Javascript Const Keyword Practice.....
+
+  /*
+  const PI = 3.141592653589793;
+  document.getElementById("Const").innerHTML 
+  = PI;
+  */
+  
+                            // Declaring a constant array does NOT make the elements unchangeable.....
+ 
+ 
+/*
+const cars = ["Saab", "Volvo", "BMW"];
+cars[0] = "Toyota";
+cars.push("Audi"); //add an element
+document.getElementById("array").innerHTML
+= cars;
+*/
+
+                                              //Javascript Const Block Scpoe Rule.....
+
+/*
+const  x = 10;
+{  
+const x = 2;
+}
+document.getElementById("scope").innerHTML 
+= "x is " + x;
+*/
+
+                                                //Javascript Operators.......
+
+// (+) Adding Operator......
+/*
+let x = 5;
+let y = 2;
+let z = x + y;
+document.getElementById("adding").innerHTML = z;
+*/
+
+
+
+//Assignment Operators.......
+/*
+let x = 10;
+document.getElementById("assign").innerHTML 
+= x;
+*/
+
+
+
+/*
+var x = 10;
+x += 5;
+document.getElementById("assign").innerHTML 
+= x;
+*/
+
+
+
+/*
+let x = 10;
+x -= 6;
+document.getElementById("assign").innerHTML 
+= x;
+*/
+
+
+
+
+// (*) Multiplication Operator......
+
+/*
+let x = 5;
+let y = 2;
+let z = x * y;
+document.getElementById("Multi").innerHTML 
+= z;
+*/
+
+//string operation...
+/*
+let text1 = "You are very ";
+text1 += "talented girl ";
+document.getElementById("str").innerHTML = text1;
+*/
+
